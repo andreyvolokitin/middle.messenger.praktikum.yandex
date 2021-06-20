4c651751d3011a3d655621a065d566ad0105f3df
andreyvolokitin <andreyvlktn@gmail.com>
2021-06-21 00:05:18 +0400

    stylelint.config.js => .stylelintrc.json

--- stylelint.config.js
+++
@@ -1,8 +0,0 @@
-// https://stylelint.io/user-guide/configure
-module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  rules: {
-    'at-rule-no-unknown': null, // https://github.com/stylelint/stylelint/issues/1612
-    // 'no-descending-specificity': null,
-  },
-};
